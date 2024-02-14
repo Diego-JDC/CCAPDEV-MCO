7bf34ac0be412ed434e21c4a84f64e56e5478f9c
Diego-JDC <diegojdelacruz@gmail.com>
2024-02-14 15:54:54 +0800

    added password field validation

--- javascript/main.js
+++ javascript/main.js
@@ -15,3 +15,18 @@ function showPass() {
 
     }
 }
+
+function samePass() { //checks if password and confirm password are the same
+    var password = document.getElementById("password").value;
+    var cpassword = document.getElementById("conf-password").value;
+    var nextPage = document.getElementById("register");
+    if (password != cpassword) {
+        alert("passwords should be the same!");
+        nextPage.setAttribute("action", "");
+    } else if (!password) {
+        alert("Enter a Password!!");
+        nextPage.setAttribute("action", "");
+    } else {
+        nextPage.setAttribute("action", "../html/index.html");
+    }
+}
